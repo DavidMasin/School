197d829670f0cc1d27866a088cafdbcecedb1316
David Masin <davidmasin2910@gmail.com>
2023-09-14 14:43:42 +0300

    1

--- Work2_All/MagicSquare.py
+++ Work2_All/MagicSquare.py
@@ -1,24 +1,21 @@
-import numpy as np
 import json
+
+import numpy as np
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.image import Image
-from kivy.uix.label import Label
 
 
 def is_magic_square(matrix):  # בודק האם הריבוע שניתן הוא ריבוע הקסם
-    # Check the sums of rows, columns, and diagonals
     target_sum = np.sum(matrix[0])  # לוקח סכום אחד של שורה (כי כולם צריכים להיות שווים)
 
-    if all(np.sum(matrix, 0) == target_sum) and all(
-            np.sum(matrix, 1) == target_sum):  # בודק האם כל האפשרויות של הסכום שוות אחת לשנייה
-        if np.sum(np.diag(matrix)) != target_sum or np.sum(np.diag(np.fliplr(matrix))) != target_sum:
+    if all(np.sum(matrix, 0) == target_sum) and all(np.sum(matrix, 1) == target_sum):  # בודק האם כל האפשרויות של הסכום שוות אחת לשנייה
+        if np.sum(np.diag(matrix)) != target_sum or np.sum(np.diag(np.fliplr(matrix))) != target_sum:#בודק האם האלכסונים שווים
             return False
 
         return True
-
-    # Check rows and columns
+    #אם הסכומים לא שווים אחד לשני אז זה לא ריבוע הקסם
     return False
 
 
@@ -48,11 +45,7 @@ def MakeDictionary(num):  # יוצר מילון
             print(value[0])  # value[0] זה המחרוזת של ריבוע הקסם
     return my_dict
 
-    # my_dict = {}
-    # for i in range(500000):
-    #     magic_square = MagicSquare(3).generate_square()
-    #     my_dict[i] = magic_square
-    # return my_dict
+
 
 
 def PutInJson(my_dict):  # בשביל לעדכן את המילון
@@ -64,7 +57,7 @@ class MyApp(App):  # האפליקציה
     def build(self):  # הבנאי של האפליקציה
         self.good_dic = {}  # ריבועי הקסם הטובים יהיו פה
 
-        # Create the main GridLayout with two rows
+        # יוצר את הלייאוט הראשי
         main_layout = GridLayout(rows=2)
 
         label_layout = GridLayout(cols=3)
@@ -79,40 +72,42 @@ class MyApp(App):  # האפליקציה
 
         button_layout = GridLayout(cols=1)
         # יוצרים כפתור בשביל לשנות את ריבוע הקסם
-        button = Button(text="REPLACE SQUARE")
+        button = Button(text='Change Square')
         button.bind(on_press=self.change_text)
-        button.width = 150
+        button.width = 50
         button_layout.add_widget(button)
 
-        #מוסיף את הלייאוטים למיין לייאוט
+        # מוסיף את הלייאוטים למיין לייאוט
         main_layout.add_widget(label_layout)
         main_layout.add_widget(button_layout)
 
-        #מתחיל מריבוע הקסם הראשון
+        # מתחיל מריבוע הקסם הראשון
         self.which_square = 0
         self.update_labels(self.which_square)
         return main_layout
 
-    def MakeLabels(self, label_layout):# יוצר את הלייאוט של הלייבלים
+    def MakeLabels(self, label_layout):  # יוצר את הלייאוט של הלייבלים
         self.labels = []
-        for _ in range(3):
+        for column in range(3):
             row_labels = []
-            for _ in range(3):
-                img = Image(source=r'C:\Users\user\PycharmProjects\School\Work2_All\placeholder.png')  # or use the actual name of your placeholder image
+            for row in range(3):
+                img = Image(
+                    source=r'C:\Users\user\PycharmProjects\School\Work2_All\placeholder.png')  # or use the actual name of your placeholder image
                 label_layout.add_widget(img)
                 row_labels.append(img)
             self.labels.append(row_labels)
 
     def change_text(self, instance):
         # מחליף את הריבוע הקסם
-        self.which_square = (self.which_square + 1) % len(self.good_dic)# עובר לריבוע הקסם הבא
-        self.update_labels(self.which_square)# מעדכן את הלייבלים
+        self.which_square = (self.which_square + 1) % len(self.good_dic)  # עובר לריבוע הקסם הבא
+        self.update_labels(self.which_square)  # מעדכן את הלייבלים
 
-    def update_labels(self, square_index):# מעדכן את הלייבלים
-        for i in range(3):# עובר על כל הלייבלים
+    def update_labels(self, square_index):  # מעדכן את הלייבלים
+        for i in range(3):  # עובר על כל הלייבלים
             for j in range(3):
                 label = self.labels[i][j]
-                img_path = r'C:\Users\user\PycharmProjects\School\Work2_All\{}.png'.format(self.good_dic[square_index][i * 3 + j])
+                img_path = r'C:\Users\user\PycharmProjects\School\Work2_All\{}.png'.format(
+                    self.good_dic[square_index][i * 3 + j])
                 label.source = img_path
                 label.reload()
 
