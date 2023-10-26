b652659f36029e89f4e4c96276dd7299cd3afe94
David Masin <davidmasin2910@gmail.com>
2023-10-26 13:48:10 +0300

    1

--- Work4Again - TicTacToe/TicTacToe.py
+++ Work4Again - TicTacToe/TicTacToe.py
@@ -1,37 +1,60 @@
+import json
+import math
 import random
 
 
 class Comp:
+    def load_dict_from_json(self):
+        with open('Dict.json', 'r') as json_file:
+            # Use a custom decoding function to preserve the tuple data type
+            return json.load(json_file, object_hook=lambda item: {k: tuple(v) for k, v in item.items()})
     def __init__(self, game_instance):
         self.game = game_instance
         self.board = self.game.getBoard()
-        if self.game.starter == 1:
+        if self.game.getStarter() == 1:
             self.shape = 'X'
         else:
             self.shape = 'O'
+        self.Dict = self.load_dict_from_json()
 
     def getTurn(self):
-        availableMoves = self.game.getAvailableMoves(self.board)
-        if availableMoves:
-            move = random.choice(availableMoves)
-            self.board = self.board.replace(move, self.shape)
-            self.game.setBoard(self.board)
-            return self.board
+
+        board = self.getBestTurn()
+        self.board = board
+        self.game.setBoard(board)
+        return self.board
+
+    def getBestTurn(self):
+        self.board = self.game.getBoard()
+        availableBoards = self.game.getAvailableBoards(self.board, self.shape)
+        maxBoard = 0
+        bestBoard = availableBoards[0]
+        if availableBoards:
+            for board in availableBoards:
+                if board in self.Dict:
+                    if self.game.Dict[board][0] > maxBoard:
+                        bestBoard = board
+                        maxBoard = self.game.Dict[board][0]
+        return bestBoard
+
 class Human:
-    def __init__(self, game_instance, starter):
+    def __init__(self, game_instance):
         self.game = game_instance
         self.board = self.game.getBoard()
-        if starter == 0:
+        self.starter = self.game.getStarter()
+        if self.starter == 0:
             self.shape = 'X'
         else:
             self.shape = 'O'
 
+
     def getTurn(self):
-        availableMoves = self.game.getAvailableBoards(self.board)
-        if availableMoves:
-            move = random.choice(availableMoves)
-            self.board = self.board.replace(move, self.shape)
-            self.game.setBoard(self.board)
+        self.board = self.game.getBoard()
+        availableBoards = self.game.getAvailableBoards(self.board, self.shape)
+        if availableBoards:
+            board = random.choice(availableBoards)
+            self.board = board
+            self.game.setBoard(board)
             return self.board
 
 
@@ -39,51 +62,79 @@ class Game:
     def __init__(self):
         self.boards = []
         self.starter = random.randrange(0, 2)
-        self.board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-        self.Computer = Comp(self,self.starter)
-        self.Human = Human(self,self.starter)
+        # print(self.starter)
+        self.board = '123456789'  # Store the board as a string
+        self.Computer = Comp(self)
+        self.Human = Human(self)
         self.ComputerWin = False
 
     def startGame(self):
         if self.starter == 1:
-            # print("Computer starts")
+            print("Computer starts")
             return self.CompStarts()
         else:
-            # print("Human starts")
+            print("Human starts")
             return self.HumanStarts()
 
     def CompStarts(self):
-        while len(self.getAvailableBoards(self.board)) > 0:
+        while not self.isGameOver():
+            print("Computer Turn")
             self.Computer.getTurn()
-            self.boards.append(self.board)
-            if len(self.getAvailableBoards(self.board)) == 0:
+            self.boards.append(self.convertToNormalStringForBoards(self.board))
+            self.printableBoard(self.board)  # Convert the board to a list for printing
+            if self.isGameOver():
                 break
+            print("Human Turn")
             self.Human.getTurn()
-            self.boards.append(self.board)
-        print(self.boards)
+            self.boards.append(self.convertToNormalStringForBoards(self.board))
+            self.printableBoard(self.board)  # Convert the board to a list for printing
+        if self.isWinner('X'):
+            self.ComputerWin = True
+        return self.boards, self.ComputerWin
 
     def HumanStarts(self):
-        while len(self.getAvailableBoards(self.board)) > 0:
-            self.Computer.getTurn()
-            self.boards.append(self.board)
-            if len(self.getAvailableBoards(self.board)) == 0:
-                break
+        while not self.isGameOver():
+            print("Human Turn")
             self.Human.getTurn()
-            self.boards.append(self.board)
-        print(self.boards)
+            self.boards.append(self.convertToNormalStringForBoards(self.board))
+            self.printableBoard(self.board)  # Convert the board to a list for printing
+            if self.isGameOver():
+                break
+            print("Computer Turn")
+            self.Computer.getTurn()
+            self.boards.append(self.convertToNormalStringForBoards(self.board))
+            self.printableBoard(self.board)  # Convert the board to a list for printing
+        if self.isWinner('O'):
+            self.ComputerWin = True
+        return self.boards, self.ComputerWin
 
-    def getAvailableBoards(self, board):
+    def getAvailableBoards(self, board, shape='X'):
         availableBoards = []
-        for i in range(3):
-            for j in range(3):
-                if board[i][j] != 'X' and board[i][j] != 'O':
-                    imaginaryBoard = board
-                    if self.starter == 0:
-                        imaginaryBoard[i][j] = 'O'
-                    else:
-                        imaginaryBoard[i][j] = 'X'
-                    availableBoards.append(imaginaryBoard)
+        for i in range(9):
+            if board[i] != 'X' and board[i] != 'O':
+                imaginaryBoard = board[:i] + shape + board[i + 1:]
+                availableBoards.append(imaginaryBoard)
         return availableBoards
+
+    def printableBoard(self, board):
+        for i in range(0, 9, 3):
+            print(board[i], board[i + 1], board[i + 2])
+
+    def isWinner(self, shape):
+        win_combinations = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
+        for combo in win_combinations:
+            if all(self.board[i] == shape for i in combo):
+                return True
+        return False
+
+    def isDraw(self):
+        for i in self.board:
+            if i != 'X' and i != 'O':
+                return False
+        return True
+
+    def isGameOver(self):
+        return self.isWinner('X') or self.isWinner('O') or self.isDraw()
 
     def getBoard(self):
         return self.board
@@ -91,4 +142,66 @@ class Game:
     def setBoard(self, board):
         self.board = board
 
+    def getStarter(self):
+        return self.starter
 
+    def convertToNormalStringForBoards(self, board):
+        normal_board = ''
+        for cell in board:
+            if cell == 'X':
+                normal_board += '1'
+            elif cell == 'O':
+                normal_board += '2'
+            else:
+                normal_board += '0'
+        return normal_board
+
+
+class Games:
+    def load_dict_from_json(self):
+        with open('Dict.json', 'r') as json_file:
+            # Use a custom decoding function to preserve the tuple data type
+            return json.load(json_file, object_hook=lambda item: {k: tuple(v) for k, v in item.items()})
+
+    def save_dict_to_json(self, Dict):
+        with open('Dict.json', 'w') as json_file:
+            # Use a custom encoding function to convert tuples to lists for JSON
+            json.dump(Dict, json_file, default=lambda item: {k: list(v) for k, v in item.items()})
+
+    def __init__(self):
+        self.Dict = self.load_dict_from_json()
+
+        game = Game()
+        boards, compWon = game.startGame()
+        currentValue = 0
+        currentTimes = 0
+        counter = 0
+        print(boards)
+        print(compWon)
+
+        for board in boards:
+            if board in self.Dict:
+                if compWon:
+                    currentValue = self.Dict[board][0]
+                    currentTimes = self.Dict[board][1]
+                    self.Dict[board] = tuple(
+                        (((currentValue * currentTimes) + math.pow(0.9, counter)) / (currentTimes + 1),
+                         (currentTimes + 1)))
+                else:
+                    currentValue = self.Dict[board][0]
+                    currentTimes = self.Dict[board][1]
+                    self.Dict[board] = tuple(((currentValue * currentTimes) / (currentTimes + 1), currentTimes + 1))
+            else:
+                if compWon:
+                    self.Dict[board] = tuple((math.pow(0.9, counter), 1))
+                else:
+                    self.Dict[board] = tuple((0, 1))
+            counter += 1
+
+        # print(self.Dict)
+        self.save_dict_to_json(self.Dict)
+
+
+if __name__ == '__main__':
+    for i in range(1):
+        Games()
