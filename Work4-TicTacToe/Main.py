e7cabd729f7591d2e2ac79c00dd30e7876482634
David Masin <davidmasin2910@gmail.com>
2023-10-26 13:48:04 +0300

    New TicTacToe

--- Work4-TicTacToe/Main.py
+++
@@ -1,8 +0,0 @@
-import Games
-
-if __name__ == '__main__':
-    for i in range(100000):
-        games = Games.Games()
-        games.startNewGame()
-        if i % 1000 == 0:
-            print(str(i / 1000) + "%")
